7bc5d8256521504c052adc391ce18a13b1f9f76f
AgafiaB <bowdenaa@jacks.sfasu.edu>
2025-04-24 19:08:18 -0500

    I adjusted the dataset class in data_helper to allow for train, val, test splits
    I also just finished setting up wandb

--- Notes.md
+++ Notes.md
@@ -1,3 +1,13 @@
 ### Questions to Ask
 * Is the SQLDataset class sufficiently efficient?
 * Is involution on the deeper layers (the ones with more channels and less spatial dims) a good idea? - yes
+
+
+### Making the Train/Dev/Test Split
+* Since we have 18,032 images, we can have a split of 90/5/5, which gives roughly 900 images each in the val and test
+* So, this means that every 2 out of 20 (1 for val and 1 for test) will go to val and test 
+* I can use 
+    SELECT * FROM table_name WHERE MOD(idx, 20) < 18 -- 90% for training
+    SELECT * FROM table_name WHERE MOD(idx, 20) = 0 -- 5% for val
+    SELECT * FROM table_name WHERE MOD(idx, 20) > 18 -- 5% for test 
+
--- data_helper.py
+++ data_helper.py
@@ -10,7 +10,7 @@ import logging
 # create a Dataset class to retrieve the data
 class SQLDataset_Informative(Dataset):
     def __init__(self, conn, label_col, img_col='image_path', data_dir=Path(os.path.expanduser('~'), 'CrisisMMD_v2.0','CrisisMMD_v2.0'), 
-                 transform=None, target_transform=None):
+                 transform=None, target_transform=None, is_train=False, is_test=False, is_val=False):
         '''
         Parameters: 
             conn - a mysql.connector object that will be used to retrieve a cursor 
@@ -19,25 +19,65 @@ class SQLDataset_Informative(Dataset):
             data_dir - the path that contains the folder containing the image paths in the sql database 
             transform - pytorch image transformations that transform the data
             target_transform - does nothing as of now, so do not specify this
+            is_train | is_val | is_test - choose none or one of these; if none chosen, all data is used 
+        
+        Notes:
+            is_train uses 90% of the data
+            is_val and is_test each use 5% of the data 
         '''
+        assert(not ((is_train and is_test) or (is_train and is_val) or (is_val and is_test)), 'a dataset can only be one of either train, test, or val')
+
         self.conn = conn
         self.img_col = img_col
         self.label_col = label_col
         self.transform = transform
         self.target_transform = target_transform
         self.data_dir = data_dir
+        self.is_train = is_train
+        self.is_val = is_val
+        self.is_test = is_test
+
+        # we need a list of available indices 
+        # what idxs are available to use for this database? - depends on the dataset type
+        if not (self.is_train or self.is_val or self.is_test): # if no dataset type specified
+            self.possible_sql_idxs = range(self.__len__())
+        else:
+            cursor = self.conn.cursor()
+            query = 'SELECT COUNT(image_id) FROM Images'
+            cursor.execute(query)
+            count = cursor.fetchone()
+
+            # below, we use +1 because SQL indexing starts at 1
+            if is_train: 
+                self.possible_sql_idxs = [idx+1 for idx in range(count) if (idx+1 % 20) < 18]
+            elif is_val:
+                self.possible_sql_idxs = [idx+1 for idx in range(count) if (idx+1 % 20) == 18]
+            else: # must be test
+                self.possible_sql_idxs = [idx+1 for idx in range(count) if (idx+1 % 20) > 18]
+            cursor.close()
 
     def __len__(self):
         '''
         Returns the number of images in the database 
         '''
-        cursor = self.conn.cursor()
-        query = 'SELECT COUNT(image_id) FROM Images'
-        cursor.execute(query)
-        count = cursor.fetchone()
-        cursor.close()
+        # cursor = self.conn.cursor()
 
-        return count[0]
+        # if not (self.is_train or self.is_val or self.is_test): # if no dataset type specified 
+        #     query = 'SELECT COUNT(image_id) FROM Images'
+        # elif self.is_train: 
+        #     query = 'SELECT COUNT(image_id) FROM Images WHERE MOD(idx, 20) < 18'
+        # elif self.is_val:
+        #     query = 'SELECT COUNT(image_id) FROM Images WHERE MOD(idx, 20) = 18'
+        # else: # must be test
+        #     query = 'SELECT COUNT(image_id) FROM Images WHERE MOD(idx, 20) > 18'
+
+        # cursor.execute(query)
+        # count = cursor.fetchone()
+        # cursor.close()
+
+        # return count[0]
+
+        return len(self.possible_sql_idxs)
     
     def __getitem__(self, idx):
         '''
@@ -47,28 +87,28 @@ class SQLDataset_Informative(Dataset):
         # retrieve an image from the sql database
 
         cursor = self.conn.cursor()
-        try:
-            query = f'SELECT {self.img_col}, {self.label_col} FROM Images WHERE idx={idx+1}' # we must add one because python starts at 0 idx but sql starts at 1
-            cursor.execute(query)
-            
-            # read in image
-            img_path, label = cursor.fetchone()
-            img_path = Path(self.data_dir, img_path)
-            image = decode_image(img_path, mode='RGB') # returns (Tensor[image_channels, image_height, image_width])
 
-            
-            if label == 'informative':
-                label = torch.tensor(1)
-            else:
-                label = torch.tensor(0)
-            # print(f'image shape before transform: {image.shape}')
-            # apply transforms on image 
-            if self.transform:
-                image = self.transform(image)
-            if self.target_transform:
-                label = self.target_transform(label)
+        try:
+                query = f'SELECT {self.img_col}, {self.label_col} FROM Images WHERE idx={self.possible_sql_idxs[idx]}' 
+                cursor.execute(query)
+                
+                # read in image
+                img_path, label = cursor.fetchone()
+                img_path = Path(self.data_dir, img_path)
+                image = decode_image(img_path, mode='RGB') # returns (Tensor[image_channels, image_height, image_width])
+
+                
+                if label == 'informative':
+                    label = torch.tensor(1)
+                else:
+                    label = torch.tensor(0)
+                # print(f'image shape before transform: {image.shape}')
+                # apply transforms on image 
+                if self.transform:
+                    image = self.transform(image)
+                if self.target_transform:
+                    label = self.target_transform(label)
         finally:
             cursor.close()
-
 
         return image, label
