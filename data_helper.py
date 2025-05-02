8acd1b8aefd2bed09380eca6b101929fec6f446a
AgafiaB <bowdenaa@jacks.sfasu.edu>
2025-05-02 15:58:32 -0500

    added the grad cam functions to data_helper.py - still need to test once the model is properly trained

--- data_helper.py
+++ data_helper.py
@@ -5,6 +5,8 @@ from torch.utils.data import Dataset
 import os
 import torch
 import logging
+import numpy as np
+import cv2
 
 
 # create a Dataset class to retrieve the data
@@ -118,3 +120,61 @@ class SQLDataset_Informative(Dataset):
             cursor.close()
 
         return {'image': image, 'label': label}
+
+
+# GRAD CAM FUNCTIONS 
+
+def get_conv_layer(model, conv_layer_name):
+    for name, layer in model.named_modules():
+        if name == conv_layer_name:
+            return layer
+        
+    raise ValueError(f'Layer {(conv_layer_name)} not found in the model')
+
+# TODO: ensure that features.18 is the correct layer for your model
+def compute_gradcam(model, img_tensor, class_idx, conv_layer_name='features.18'):
+    conv_layer = get_conv_layer(model, conv_layer_name)
+
+    # fwd hook to store activations
+    # this will give us a feature map to use for our grad cam
+    activations = None
+    def forward_hook(module, input, output):
+        nonlocal activations # COMMENT: what does this even mean??
+        activations = output
+
+    hook = conv_layer.register_forward_hook(forward_hook)
+
+    # compute gradients
+    img_tensor.requires_grad_(True)
+    preds = model(img_tensor)
+    loss = preds[:, class_idx]
+    model.zero_grad()
+    loss.backward()
+
+    grads = img_tensor.grad.cpu().numpy()
+    pooled_grads = np.mean(grads, axis=(0, 2, 3))
+
+    # remove the hook
+    hook.remove()
+
+    activations = activations.detach().cpu().numpy()[0]
+    for i in range(pooled_grads.shape[0]):
+        activations[i, ...] *= pooled_grads[i]
+
+    heatmap = np.mean(activations, axis=0)
+    heatmap = np.maximum(heatmap, 0) # essentially a RELU activation fn
+    heatmap /= np.max(heatmap)
+
+    return heatmap
+
+def overlay_heatmap(img_path, heatmap, alpha=.4):
+    '''
+    Returns a cv2 image with the heatmap superimposed onto it 
+    '''
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255*heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    
+    superimposed_img = cv2.addWeighted(img, alpha, heatmap, 1 - alpha, 0)
+    return superimposed_img
